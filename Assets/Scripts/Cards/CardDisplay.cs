d131073625a1247a0d7e4bf9897ee19c9bad5ebd
Veith M. Bürgerhoff <veith.buergerhoff@gmail.com>
2024-04-27 20:41:53 +0200

    more refactoring

--- Assets/Scripts/Cards/CardDisplay.cs
+++ Assets/Scripts/Cards/CardDisplay.cs
@@ -7,16 +7,16 @@ using UnityEngine.UI;
 public class CardDisplay : MonoBehaviour
 {
     public GameObject cardDisplay;
-
-    private List<Tuple<Card, CardController>> cardDisplays = new();
     public float cardWidth;
 
     public event Action<CardController> CardClicked;
 
+    private List<CardController> cardDisplays = new();
+
     public void AddCard(Card card)
     {
         var image = Instantiate(cardDisplay).GetComponent<CardController>();
-        cardDisplays.Add(Tuple.Create(card, image));
+        cardDisplays.Add(image);
         image.GetComponent<Image>().sprite = card.image;
         image.transform.SetParent(transform);
 
@@ -28,12 +28,12 @@ public class CardDisplay : MonoBehaviour
 
     public void RemoveCard(Card card)
     {
-        var item = cardDisplays.Where(x => x.Item1 == card).FirstOrDefault();
+        var item = cardDisplays.Where(x => x.card == card).FirstOrDefault();
         if (item != null)
         {
             cardDisplays.Remove(item);
-            item.Item2.CardClicked -= CardClicked;
-            Destroy(item.Item2);
+            item.CardClicked -= CardClicked;
+            Destroy(item);
             Rerender();
         }
     }
@@ -45,7 +45,7 @@ public class CardDisplay : MonoBehaviour
             var width = cardWidth - cardDisplays.Count;
             var shift = width * (cardDisplays.Count - 1) / 2;
             var newPosition = transform.position + Vector3.right * (i * width - shift);
-            cardDisplays[i].Item2.transform.position = newPosition;
+            cardDisplays[i].transform.position = newPosition;
         }
     }
 }
